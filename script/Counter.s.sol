ff166bac3cc0f908c41d3ed1f627986b62af8cc4
Ahlemmer <ahlemmerabtene@gmail.com>
2024-08-11 21:21:12 +0200

    Lottery-setup

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {Raffel} from "../src/Raffel.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
+    Raffel public counter;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        counter = new Raffel(12);
 
         vm.stopBroadcast();
     }
---
+++ src/Raffel.sol
@@ -0,0 +1,126 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+/**
+ * @title A sample Raffle Contract
+ * @author Ahlem Merabtene
+ * @notice This contract is for creating a sample raffle contract
+ * @dev This implements the Chainlink VRF Version 2
+ */
+
+contract Raffle is VRFConsumerBaseV2Plus {
+    //Error
+    error Raffel__SendMoreEther();
+    error Raffle__TransferFailed();
+    error  Raffle__RaffleNotOpen();
+    
+
+     enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    address private s_recentWinner;
+
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    RaffleState private s_raffleState;
+
+    event RaffelEntred(address indexed player);
+
+    constructor(
+        uint256 subscriptionId,
+        bytes32 gasLane, // keyHash
+        uint256 interval,
+        uint256 entranceFee,
+        uint32 callbackGasLimit,
+        address vrfCoordinatorV2
+    ) VRFConsumerBaseV2Plus(vrfCoordinatorV2) {
+        i_gasLane = gasLane;
+        i_interval = interval;
+        i_subscriptionId = subscriptionId;
+        i_entranceFee = entranceFee;
+        s_lastTimeStamp = block.timestamp;
+        i_callbackGasLimit = callbackGasLimit;
+       s_raffleState = RaffleState.OPEN;
+    }
+
+    function entranceRaffel() external payable {
+        // require(msg.value<i_entranceFee,"Send More Eth"); it's expensive
+        if (msg.value < i_entranceFee) {
+            revert Raffel__SendMoreEther();
+        }
+         if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__RaffleNotOpen();
+        }
+        // require(msg.value<i_entranceFee,SendMoreEther());
+        s_players.push(payable(msg.sender));
+        emit RaffelEntred(msg.sender);
+    }
+
+    function fulfillRandomWords(uint256, /* requestId */ uint256[] calldata randomWords) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+         s_recentWinner = recentWinner;
+         s_raffleState = RaffleState.OPEN;
+          (bool success,) = recentWinner.call{value: address(this).balance}("");
+        // require(success, "Transfer failed");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+    }
+
+    function pickWinr() external {
+        if ((block.timestamp - s_lastTimeStamp) > i_interval) {
+            revert();
+        }
+        s_raffleState = RaffleState.CALCULATING;
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_gasLane,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+    }
+
+    function getRaffel() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
